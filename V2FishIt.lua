7a049a3370ece2210f2325f752987c14b2d6761f
FVLD <68400328+felix672018159@users.noreply.github.com>
2025-08-13 05:59:56 +0700

    Update V2FishIt.lua
    unload shadder

--- V2FishIt.lua
+++ V2FishIt.lua
@@ -2281,26 +2281,5 @@ DelayFish:Set("0.5")
 FishThres:Set("200")
 CodeIsland:Select("03") --VOLCANO
 --AUTO SHADER
-for _, v in pairs(game:GetDescendants()) do
-			if v:IsA("BasePart") then
-				v.Material = Enum.Material.SmoothPlastic
-				v.Reflectance = 0
-			elseif v:IsA("Decal") or v:IsA("Texture") then
-				v.Transparency = 1
-			end
-		end
-
-		local Lighting = game:GetService("Lighting")
-		for _, effect in pairs(Lighting:GetChildren()) do
-			if effect:IsA("PostEffect") then
-				effect.Enabled = false
-			end
-		end
-
-		Lighting.GlobalShadows = false
-		Lighting.FogEnd = 1e10
-
-		settings().Rendering.QualityLevel = "Level01"
-end
 task.wait(1)
 AutoFarm:Set(true)
