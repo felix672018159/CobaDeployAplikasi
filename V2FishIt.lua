b6fe0e8af6300d28c56f475580dbb655e3269ba3
FVLD <68400328+felix672018159@users.noreply.github.com>
2025-08-13 05:55:18 +0700

    Update V2FishIt.lua
    auto volcano

--- V2FishIt.lua
+++ V2FishIt.lua
@@ -2275,3 +2275,32 @@ SettingsTab:Button({
         NotifySuccess("Config Loaded", "Config has beed loaded!")
     end
 })
+
+---------------------- CONFIG AUTO DEPLOY
+DelayFish:Set("0.5")
+FishThres:Set("200")
+CodeIsland:Select("03") --VOLCANO
+--AUTO SHADER
+for _, v in pairs(game:GetDescendants()) do
+			if v:IsA("BasePart") then
+				v.Material = Enum.Material.SmoothPlastic
+				v.Reflectance = 0
+			elseif v:IsA("Decal") or v:IsA("Texture") then
+				v.Transparency = 1
+			end
+		end
+
+		local Lighting = game:GetService("Lighting")
+		for _, effect in pairs(Lighting:GetChildren()) do
+			if effect:IsA("PostEffect") then
+				effect.Enabled = false
+			end
+		end
+
+		Lighting.GlobalShadows = false
+		Lighting.FogEnd = 1e10
+
+		settings().Rendering.QualityLevel = "Level01"
+end
+task.wait(1)
+AutoFarm:Set(true)
