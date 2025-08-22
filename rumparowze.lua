0d3672e8424a9a4aa5a9914487fa1052e227403c
FVLD <68400328+felix672018159@users.noreply.github.com>
2025-08-22 11:43:57 +0700

    Update rumparowze.lua

--- rumparowze.lua
+++ rumparowze.lua
@@ -18,7 +18,7 @@ if isBootedYet then
     error("Well, STOP HERE !")
 end
 ---------------------- CHECK PLAYER
-local usernameLocked = "rumparowze|orazovomez|bagsadzze|supezover|sofortezoo|fritzwezero|troazxvozer" -- raw won't updated
+local usernameLocked = "rumparowze|orazovomez|bagsadzze|supezover|sofortezoo|fritzwezero|troazxvozer|Agus_berakkacang" -- raw won't updated
 local playerMeow = game:GetService("Players")
 local usernameCheck = playerMeow.LocalPlayer.DisplayName
 local usernameA = string.upper(usernameLocked)
