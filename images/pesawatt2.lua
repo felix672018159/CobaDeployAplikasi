77b6c2e34f8ea4c04ae668a0522cf27fcddb9dce
FVLD <68400328+felix672018159@users.noreply.github.com>
2025-09-05 11:00:12 +0700

    Update pesawatt2.lua

--- images/pesawatt2.lua
+++ images/pesawatt2.lua
@@ -772,7 +772,7 @@ function StartAutoFishV2()
                     RodReel:Stop()
                     forceCloseTime = true
                     isCaughtFishWhenStartedAutoFish = false
-					task.wait(10)
+					task.wait(5)
                     break
                   end
                 until isCaughtFishWhenStartedAutoFish == true
